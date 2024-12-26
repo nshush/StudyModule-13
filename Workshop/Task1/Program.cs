cb6e30b75030634fd7f353f3d497e144bd555b51
nshush <kagekao137@gmail.com>
2024-12-26 21:44:54 +0300

    Add Tasks 1 & 2

---
+++ Workshop/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к файлу:");
+        string filePath = Console.ReadLine();
+
+        List<int> listData = new List<int>();
+        LinkedList<int> linkedListData = new LinkedList<int>();
+
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+                listData.Add(int.Parse(line));
+        }
+
+        Stopwatch stopwatch = new Stopwatch();
+
+        Console.WriteLine("Вставка в List<T>:");
+        stopwatch.Start();
+        for (int i = 0; i < 1000; i++)
+        {
+            listData.Insert(0, i); 
+        }
+        stopwatch.Stop();
+        TimeSpan timeSpan = stopwatch.Elapsed;
+        Console.WriteLine($"Время выполнения: {timeSpan.TotalMilliseconds} ms");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Вставка в LinkedList<T>:");
+        stopwatch.Reset();
+        stopwatch.Start();
+        for (int i = 0; i < 1000; i++)
+        {
+            linkedListData.AddFirst(i); 
+        }
+        stopwatch.Stop();
+        timeSpan = stopwatch.Elapsed;
+        Console.WriteLine($"Время выполнения: {timeSpan.TotalMilliseconds} ms");
+    }
+}
---
+++ Workshop/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к файлу:");
+        string filePath = Console.ReadLine();
+
+        string text = File.ReadAllText(filePath);
+
+        var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+        string[] words = noPunctuationText.Split(' ');
+
+        Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+        foreach (string word in words)
+        {
+            if (wordFrequency.ContainsKey(word))
+                wordFrequency[word]++;
+            else
+                wordFrequency.Add(word, 1);
+        }
+
+        var sortedWordFrequency = from pair in wordFrequency
+                                  orderby pair.Value descending
+                                  select pair;
+
+        Console.WriteLine("10 самых частых слов:");
+        int i = 0;
+        foreach (KeyValuePair<string, int> pair in sortedWordFrequency)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+            i++;
+            if (i == 10)
+                break;
+        }
+    }
+}
